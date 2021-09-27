81019122ae5be152c3292748d7678691b9252920
unknown <shaik.shah@northeastern.edu>
2021-09-27 08:48:17 +0530

    commiting

---
+++ AED_Assignment/src/model/Product.java
@@ -0,0 +1,209 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Shahla
+ */
+public class Product {
+    
+    private String name;
+    private String description;
+    private String availNum;
+    private String price;
+    
+    
+    //New Variables
+    
+    private String geoData;
+    private String dob;
+    private String teleNum;
+    private String faxNum;
+    private String emailAddress;
+    private String ssn;
+    private String medicalRNum;
+    private String healthPlanNumber;
+    private String bankAccNum;
+    private String licenseNum;
+    private String vehicleNum;
+    private String devSerialNum;
+    private String linkedIn;
+    private String iPAddress;
+    private String bioId;
+    private String images;
+    private String uINum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailNum() {
+        return availNum;
+    }
+
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTeleNum() {
+        return teleNum;
+    }
+
+    public void setTeleNum(String teleNum) {
+        this.teleNum = teleNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRNum() {
+        return medicalRNum;
+    }
+
+    public void setMedicalRNum(String medicalRNum) {
+        this.medicalRNum = medicalRNum;
+    }
+
+    public String getHealthPlanNumber() {
+        return healthPlanNumber;
+    }
+
+    public void setHealthPlanNumber(String healthPlanNumber) {
+        this.healthPlanNumber = healthPlanNumber;
+    }
+
+    public String getBankAccNum() {
+        return bankAccNum;
+    }
+
+    public void setBankAccNum(String bankAccNum) {
+        this.bankAccNum = bankAccNum;
+    }
+
+    public String getLicenseNum() {
+        return licenseNum;
+    }
+
+    public void setLicenseNum(String licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+
+    public String getVehicleNum() {
+        return vehicleNum;
+    }
+
+    public void setVehicleNum(String vehicleNum) {
+        this.vehicleNum = vehicleNum;
+    }
+
+    public String getDevSerialNum() {
+        return devSerialNum;
+    }
+
+    public void setDevSerialNum(String devSerialNum) {
+        this.devSerialNum = devSerialNum;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getiPAddress() {
+        return iPAddress;
+    }
+
+    public void setiPAddress(String iPAddress) {
+        this.iPAddress = iPAddress;
+    }
+
+    public String getBioId() {
+        return bioId;
+    }
+
+    public void setBioId(String bioId) {
+        this.bioId = bioId;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getuINum() {
+        return uINum;
+    }
+
+    public void setuINum(String uINum) {
+        this.uINum = uINum;
+    }
+    
+
+    
+}
